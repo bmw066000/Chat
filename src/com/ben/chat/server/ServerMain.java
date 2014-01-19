8f6620222be8fec08aa2261fb57b43aec218cd9e
Ben Wright <bmw066000@gmail.com>
2014-01-19 17:11:58 -0500

    Episode 15
    Server Threads and Explanations

--- src/com/ben/chat/server/ServerMain.java
+++ src/com/ben/chat/server/ServerMain.java
@@ -3,9 +3,11 @@ package com.ben.chat.server;
 public class ServerMain {
 	
 	private int port;
+	private Server server;
 	
 	public ServerMain(int port) {
 		this.port = port;
+		server = new Server(port);
 	}
 	
 	public static void main(String[] args) {
