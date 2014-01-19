0f3696f228c27baa193c7d32675a6a5cbd860a57
Ben Wright <bmw066000@gmail.com>
2014-01-19 11:45:33 -0500

    Episode 2
    The Login Window

--- src/com/ben/chat/Login.java
+++ src/com/ben/chat/Login.java
@@ -1,24 +1,41 @@
 package com.ben.chat;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Login extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private JPanel contentPane;
+	private JTextField txtName;
 
 	public Login() {
+		setResizable(false);
+		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setSize(300, 380);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 13));
+		txtName.setBounds(64, 68, 165, 20);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblName = new JLabel("Name:");
+		lblName.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 13));
+		lblName.setBounds(122, 50, 49, 14);
+		contentPane.add(lblName);
 	}
 
 	public static void main(String[] args) {
@@ -33,5 +50,4 @@ public class Login extends JFrame {
 			}
 		});
 	}
-	
 }
